8b1b5fd749f978805820559218af3d2de6d20cd7
RitaChio <44211750+RitaChio@users.noreply.github.com>
2021-01-19 14:42:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function function1() {
+    document.getElementById("input1")
+    }
+}
+
+function function2() {
+    document.getElementById("input2")
+}
+
+function function3() {
+    document.getElementById("input3")
+}
+
+function funciton4(){
+    document.getElementById("input4")
+}
+
+function function5(){
+    document.getElementById("input5")
+}
